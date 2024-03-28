768033d082ac84beb7bf79d78c17cfcf4a42be10
Tim Sheridan <tghs@tghs.net>
2024-03-28 22:11:59 +0200

    Disable GRUB graphical mode in installed OS, and time out ASAP (press Esc to interrupt)

--- config/includes.installer/install_late_command.sh
+++ config/includes.installer/install_late_command.sh
@@ -4,3 +4,15 @@ cat << EOF > /target/etc/apt/sources.list
 deb http://deb.debian.org/debian bookworm main contrib non-free-firmware
 EOF
 
+mv /target/etc/default/grub /target/etc/default/grub.example
+cat << EOF > /target/etc/default/grub
+GRUB_TIMEOUT_STYLE=hidden
+GRUB_HIDDEN_TIMEOUT=0
+GRUB_DEFAULT=0
+GRUB_TIMEOUT=0
+GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
+GRUB_CMDLINE_LINUX_DEFAULT="quiet"
+GRUB_CMDLINE_LINUX="initrd=/install/gtk/initrd.gz"
+GRUB_TERMINAL=console
+EOF
+in-target update-grub
